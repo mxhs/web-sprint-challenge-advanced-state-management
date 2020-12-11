e5eeba6263819dce7c50e322d824ac9d5c91977d
mxhs <maxwellstofman@gmail.com>
2020-12-11 10:58:44 -0800

    Store almost completed

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,23 @@
+//Dependencies
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Components/Reducers
+import App from "./components/App";
+import {smurfsReducer} from './store/reducers/smurfsReducer'
+
+//Styles
+import "./index.css";
+
+const store = createStore(smurfsReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById("root")
+);
